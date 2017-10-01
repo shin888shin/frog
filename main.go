28d35181aeff5ed3f0ed47ccf091cc9bc258bf75
Joel Shin <jwshin@gmail.com>
2017-09-30 23:31:40 -0700

    cleaned up get users

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
-	// "log"
 	"database/sql"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/shin888shin/frog/db"
 	"html/template"
+	"log"
 	"net/http"
-	"strconv"
 )
 
-var err error
+type Todo struct {
+	Id          int
+	Name        string
+	Description string
+}
 
-// var dbConfig db.Database
+var err error
 var dbConn *sql.DB
+var t *template.Template
 
 func main() {
 	http.HandleFunc("/", handleRoot)
@@ -22,22 +25,14 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-var t *template.Template
-
 func init() {
-	fmt.Println("Initialize")
-
 	dbConn, err = db.Connection(dbConn)
-	fmt.Println("---> init dbConn:", dbConn)
-
 	if err != nil {
 		panic(err)
 	}
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
-	// fmt.Println("---> handleRoot dbConn:", dbConn)
-
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -50,20 +45,8 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-type Todo struct {
-	Id          int
-	Name        string
-	Description string
-}
-
 func handleAllTodos(w http.ResponseWriter, r *http.Request) {
-	// fmt.Println("---> handleAllTodos dbConn:", dbConn)
-	// todos := []Todo{}
-
 	todos := showAllTodos()
-	for _, t := range todos {
-		fmt.Println("ooo> ", t)
-	}
 	if r.URL.Path != "/todos" {
 		http.NotFound(w, r)
 		return
@@ -78,73 +61,28 @@ func handleAllTodos(w http.ResponseWriter, r *http.Request) {
 }
 
 func showAllTodos() []Todo {
-	rows, err := dbConn.Query("SELECT * FROM todos")
+	var (
+		id          int
+		name        string
+		description string
+	)
+	rows, err := dbConn.Query("SELECT id, name, description FROM todos")
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err)
 	}
+	defer rows.Close()
 
-	var count int
+	var todos = make([]Todo, 0)
 	for rows.Next() {
-		count += 1
-	}
-	fmt.Println("+++> count:", count)
-
-	rows, err = dbConn.Query("SELECT * FROM todos")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	// fmt.Printf("---> rows: %T - %v\n", rows, rows)
-
-	columns, err := rows.Columns()
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
-	// fmt.Printf("---> columns: %T - %v\n", columns, columns)
-	values := make([]sql.RawBytes, len(columns))
-	scanArgs := make([]interface{}, len(values))
-	for i := range values {
-		scanArgs[i] = &values[i]
-	}
-
-	// fmt.Println("+++> len of values:", len(rows))
-	fmt.Println("+++> count 2:", count)
-	var todos = make([]Todo, 1)
-
-	for rows.Next() {
-		fmt.Println("+++> row")
-		err = rows.Scan(scanArgs...)
+		err := rows.Scan(&id, &name, &description)
 		if err != nil {
-			panic(err.Error()) // proper error handling instead of panic in your app
+			log.Fatal(err)
 		}
-
-		var value string
-		var todo Todo
-		for i, col := range values {
-			// Here we can check if the value is nil (NULL value)
-			if col == nil {
-				value = "NULL"
-			} else {
-				// fmt.Printf("---> col: %T - %v\n", col, col)
-				if i == 0 {
-					value = string(col)
-					x, _ := strconv.Atoi(value)
-					todo.Id = x
-				} else if i == 1 {
-					value = string(col)
-					todo.Name = value
-				} else {
-					value = string(col)
-					todo.Description = value
-				}
-			}
-		}
-		fmt.Println("todo:", todo)
-		todos = append(todos, todo)
-		// fmt.Println("-----------------------------------")
+		todos = append(todos, Todo{Id: id, Name: name, Description: description})
 	}
-	if err = rows.Err(); err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
 	}
 	return todos
 }
