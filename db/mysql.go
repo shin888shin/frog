00288466b6821e958cc92909dfd42b21e4d10530
Joel Shin <jwshin@gmail.com>
2017-09-30 19:35:50 -0700

    show users

--- db/mysql.go
+++ db/mysql.go
@@ -1,92 +1,53 @@
 package db
 
 import (
-  "fmt"
-  // "log"
-  "io/ioutil"
-  "strings"
-  "database/sql"
-  _ "github.com/go-sql-driver/mysql"
-
+	// "log"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"io/ioutil"
+	"strings"
 )
 
 type DbCreds struct {
-  User string
-  Password string
-  Database string
-  Host string
+	User     string
+	Password string
+	Database string
+	Host     string
 }
+
 func getDbCreds() (DbCreds, error) {
-  var creds DbCreds
-  fileName := "config/local_mysql.txt"
-  contentBytes, err := ioutil.ReadFile(fileName)
+	var creds DbCreds
+	fileName := "config/local_mysql.txt"
+	contentBytes, err := ioutil.ReadFile(fileName)
 
-  if err != nil {
-    return creds, err
-  }
-  result := string(contentBytes)
-  lines := strings.Split(result, "\n")
+	if err != nil {
+		return creds, err
+	}
+	result := string(contentBytes)
+	lines := strings.Split(result, "\n")
 
-  for i, line := range lines {
-    val := strings.TrimSpace(line)
-    if i == 0 {
-      creds.User = val
-    } else if i == 1 {
-      creds.Password = val
-    } else if i == 2 {
-      creds.Database = val
-    } else {
-      creds.Host = val
-    }
-  }
-  return creds, nil
-
-}
-func Zzz() (*sql.DB, error) {
-
-
-  creds, err := getDbCreds()
-
-  creds_str := creds.User + ":" + creds.Password + "@/" + creds.Database
-  fmt.Println("Creds:", creds_str)
-
-  db, err := sql.Open("mysql", creds_str)
-  // defer db.Close()
-  fmt.Printf("Initializezzzxx mysql %T - %v\n", db, db)
-
-  // dbConfig, err = db.DatabaseSetup()
-  if err != nil {
-    panic(err)
-  }
-  return db, nil
+	for i, line := range lines {
+		val := strings.TrimSpace(line)
+		if i == 0 {
+			creds.User = val
+		} else if i == 1 {
+			creds.Password = val
+		} else if i == 2 {
+			creds.Database = val
+		} else {
+			creds.Host = val
+		}
+	}
+	return creds, nil
 }
 
-// func DatabaseSetup() (Database, error) {
-//   var db Database
-//   fileName := "config/local_mysql.txt"
-//   contentBytes, err := ioutil.ReadFile(fileName)
+func Connection(conn *sql.DB) (*sql.DB, error) {
+	creds, err := getDbCreds()
+	creds_str := creds.User + ":" + creds.Password + "@/" + creds.Database
 
-//   if err != nil {
-//     return db, err
-//   }
-
-//   result := string(contentBytes)
-//   lines := strings.Split(result, "\n")
-
-//   for i, line := range lines {
-//     fmt.Println("---> line:", line)
-//     val := strings.TrimSpace(line)
-//     if i == 0 {
-//       db.User = val
-//     } else if i == 1 {
-//       db.Password = val
-//     } else if i == 2 {
-//       db.Database = val
-//     } else {
-//       db.Host = val
-//     }
-//     fmt.Printf("---> db: %v\n", db)
-
-//   }
-//   return db, nil
-// }
+	conn, err = sql.Open("mysql", creds_str)
+	if err != nil {
+		panic(err)
+	}
+	return conn, nil
+}
--- main.go
+++ main.go
@@ -2,95 +2,149 @@ package main
 
 import (
 	// "log"
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/shin888shin/frog/db"
 	"html/template"
 	"net/http"
-	"github.com/shin888shin/frog/db"
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
+	"strconv"
 )
 
-// var err error
+var err error
+
 // var dbConfig db.Database
 var dbConn *sql.DB
 
 func main() {
 	http.HandleFunc("/", handleRoot)
+	http.HandleFunc("/todos", handleAllTodos)
 	http.ListenAndServe(":8080", nil)
 }
 
 var t *template.Template
 
 func init() {
-	fmt.Println("Initialize frog")
+	fmt.Println("Initialize")
 
- //  db, err := sql.Open("mysql", "root:yllis@/fish_dev")
- //  defer db.Close()
-	// fmt.Printf("Initialize mysql %T - %v\n", db, db)
+	dbConn, err = db.Connection(dbConn)
+	fmt.Println("---> init dbConn:", dbConn)
 
-	dbConn, err := db.Zzz()
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
+}
 
-  rows, err := dbConn.Query("SELECT * FROM todos")
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	// fmt.Println("---> handleRoot dbConn:", dbConn)
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	t, _ = template.ParseFiles("views/home.html", "views/layout.html")
+	err = t.ExecuteTemplate(w, "layout", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+type Todo struct {
+	Id          int
+	Name        string
+	Description string
+}
+
+func handleAllTodos(w http.ResponseWriter, r *http.Request) {
+	// fmt.Println("---> handleAllTodos dbConn:", dbConn)
+	// todos := []Todo{}
+
+	todos := showAllTodos()
+	for _, t := range todos {
+		fmt.Println("ooo> ", t)
+	}
+	if r.URL.Path != "/todos" {
+		http.NotFound(w, r)
+		return
+	}
+	t, _ = template.ParseFiles("views/todos/index.html", "views/layout.html")
+	err := t.ExecuteTemplate(w, "layout", struct{ Todos []Todo }{todos})
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func showAllTodos() []Todo {
+	rows, err := dbConn.Query("SELECT * FROM todos")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var count int
+	for rows.Next() {
+		count += 1
+	}
+	fmt.Println("+++> count:", count)
+
+	rows, err = dbConn.Query("SELECT * FROM todos")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// fmt.Printf("---> rows: %T - %v\n", rows, rows)
+
+	columns, err := rows.Columns()
 	if err != nil {
 		panic(err.Error()) // proper error handling instead of panic in your app
 	}
-
-	fmt.Printf("---> rows: %T - %v\n", rows, rows)
-
-  columns, err := rows.Columns()
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
-	fmt.Printf("---> columns: %T - %v\n", columns, columns)
+	// fmt.Printf("---> columns: %T - %v\n", columns, columns)
 	values := make([]sql.RawBytes, len(columns))
 	scanArgs := make([]interface{}, len(values))
 	for i := range values {
 		scanArgs[i] = &values[i]
 	}
 
-  for rows.Next() {
+	// fmt.Println("+++> len of values:", len(rows))
+	fmt.Println("+++> count 2:", count)
+	var todos = make([]Todo, 1)
+
+	for rows.Next() {
+		fmt.Println("+++> row")
 		err = rows.Scan(scanArgs...)
 		if err != nil {
 			panic(err.Error()) // proper error handling instead of panic in your app
 		}
 
 		var value string
+		var todo Todo
 		for i, col := range values {
 			// Here we can check if the value is nil (NULL value)
 			if col == nil {
 				value = "NULL"
 			} else {
-				value = string(col)
+				// fmt.Printf("---> col: %T - %v\n", col, col)
+				if i == 0 {
+					value = string(col)
+					x, _ := strconv.Atoi(value)
+					todo.Id = x
+				} else if i == 1 {
+					value = string(col)
+					todo.Name = value
+				} else {
+					value = string(col)
+					todo.Description = value
+				}
 			}
-			fmt.Println(columns[i], ": ", value)
 		}
-		fmt.Println("-----------------------------------")
+		fmt.Println("todo:", todo)
+		todos = append(todos, todo)
+		// fmt.Println("-----------------------------------")
 	}
 	if err = rows.Err(); err != nil {
 		panic(err.Error()) // proper error handling instead of panic in your app
 	}
- //  db, err = mysql.DialPassword(dbConfig.Host, dbConfig.User, dbConfig.Password)  
- //  if err != nil {
- //    log.Print("oh no")
- //  }
- //  _, err4 := db.Exec("use fish_dev")
- //  if err4 != nil {
- //    log.Fatal(err)
- //  }
-}
-
-func handleRoot(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	t, _ = template.ParseFiles("views/home.html", "views/layout.html")
-	err := t.ExecuteTemplate(w, "layout", nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	return todos
 }
